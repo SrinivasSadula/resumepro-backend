4610501879e92bc6ac311b3b87a8f29f3f109536
Srinivas Sadula <sadula.srinivas0000@gmail.com>
2025-08-03 15:43:26 +0530

    1.0.3 || insert and update resume function added

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -4,10 +4,14 @@ const Resume = require("../models/Resume");
 exports.saveResume = async (req, res) => {
   try {
     console.log("Saving resume", req.body);
-    // Pass the entire req.body as resumeData
-    const newResume = new Resume({ userId: req.user.id, resumeData: req.body });
-    await newResume.save().then(() => {console.log("Resume saved successfully");})
-    res.status(201).json({ message: "Resume saved" });
+    console.log("Saving resume for user:", req.user.id);
+    await Resume.findOneAndUpdate(
+      { userId: req.user.id },
+      { userId: req.user.id, resumeData: req.body },
+      { upsert: true, new: true, setDefaultsOnInsert: true }
+    );
+    console.log("Resume saved/upserted successfully");
+    res.status(201).json({ message: "Resume saved/upserted" });
   } catch (err) {
     console.log("Error saving resume:", err);
     res.status(500).json({ message: err.message });
@@ -16,8 +20,9 @@ exports.saveResume = async (req, res) => {
 
 exports.getResumes = async (req, res) => {
   try {
-    const resumes = await Resume.find({ userId: req.user.id });
-    res.status(200).json(resumes);
+    console.log("Fetching resume for user:", req.user.id);
+    const resume = await Resume.findOne({ userId: req.user.id });
+    res.status(200).json(resume);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
